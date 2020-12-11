301ed5de513342b6646def81ee96abd27422fc3e
Sean Sullivan <dev@seanmedia.ca>
2020-12-10 21:03:19 -0700

    Outline refactoring parser to use a domain-model approach -wip

---
+++ parser/pdfminer/__init__.py
@@ -0,0 +1,5 @@
+# TODO: Refactor entire parser into a project structure using a domain model, service layer and adaptors.
+
+# TODO: Add an abstract parser class that will be inherited by the pdfminer adaptor, opencv adaptor or tesseract adaptor.
+
+# TODO: Isolate the domain model from pdfminer.six and move the 'business' logic to the service layer.
