f4f72f3642620643dea126a067c194e39d2759a4
Sean Sullivan <dev@seanmedia.ca>
2020-12-11 17:33:06 -0700

    Add tasks for refactoring project to use domain model -wip

--- parser/pdfminer/__init__.py
+++ parser/pdfminer/__init__.py
@@ -3,3 +3,7 @@
 # TODO: Add an abstract parser class that will be inherited by the pdfminer adaptor, opencv adaptor or tesseract adaptor.
 
 # TODO: Isolate the domain model from pdfminer.six and move the 'business' logic to the service layer.
+
+# TODO: Write an adapter to convert pdfminer.six class models into the package's own class models.
+
+# TODO: Add a separate module for handling ocr from pdf images, where text data is not available.
